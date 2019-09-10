16a3d935bfc97ac77d1314415f6b7a49b0443e21
Kristof Toth <tothkrisi@gmail.com>
2019-09-10 11:02:17 +0200

    add description & readchar dependency

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     version='1.0',
     author='Kristof Toth',
     author_email='tothkrisi@gmail.com',
-    description='', install_requires=['pyperclip', 'colorama']
+    description='Standalone python script for generating reverse shells easily and automating the boring stuff like '
+                'URL encoding the command and setting up a listener.',
+    install_requires=['pyperclip', 'colorama', 'readchar']
 )
