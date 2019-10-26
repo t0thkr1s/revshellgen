50f3d2a6a84dd26cff3c79a5a737913fc188c9e2
Kristof Toth <tothkrisi@gmail.com>
2019-10-26 11:35:18 +0200

    add new dependency

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='revshellgen',
-    version='1.0',
+    version='1.1',
     author='Kristof Toth',
-    author_email='tothkrisi@gmail.com',
+    author_email='t0thkr1s@icloud.com',
     description='Standalone python script for generating reverse shells easily and automating the boring stuff like '
                 'URL encoding the command and setting up a listener.',
-    install_requires=['pyperclip', 'colorama', 'readchar']
+    install_requires=['pyperclip', 'colorama', 'readchar', 'netifaces']
 )
