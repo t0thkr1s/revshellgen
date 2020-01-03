280c3e1cb09bb69759666e55d033a88ae5560f7f
Mr. Robot <t0thkr1s@icloud.com>
2020-01-03 10:57:41 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='revshellgen',
     version='1.2',
-    author='Kristof Toth',
+    author='https://github.com/t0thkr1s',
     author_email='t0thkr1s@icloud.com',
     description='Standalone python script for generating reverse shells easily and automating the boring stuff like '
                 'URL encoding the command and setting up a listener.',
