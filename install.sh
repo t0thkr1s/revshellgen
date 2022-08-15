f8ed6ee8702b3e05631c6a8a3806a0d7191d03b3
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-08-15 14:53:59 +0200

    update the install, add an script to install dependancies

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git
+# install python3 and python-pip
+sudo apt-get install -y python3 python3-pip
+# install python3 dependencies from requirements.txt
+pip3 install -r requirements.txt
+# install by run the install setup.py script
+python3 setup.py install
